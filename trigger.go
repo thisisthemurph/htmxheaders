c0ead4e20ba644cc872a11d8a0f79794fe7af7a1
Mike Murphy <michael.w.muprhy@outlook.com>
2024-02-13 07:54:44 +0000

    Adds HX-Trigger-After-Swap and HX-Trigger-After-Settle

--- trigger.go
+++ trigger.go
@@ -6,9 +6,37 @@ import (
 	"strings"
 )
 
-func Trigger(eventName ...string) DecoratorFunction {
+// TriggerDelay allows distinguishing between different types of trigger headers.
+// https://htmx.org/headers/hx-trigger/
+type TriggerDelay int
+
+const (
+	TriggerImmediately TriggerDelay = iota // trigger events as soon as the response is received.
+	TriggerAfterSettle                     // trigger events after the settle step.
+	TriggerAfterSwap                       // trigger events after the swap step.
+)
+
+// String returns the name of the header associated with when the event should be triggered.
+// Possible values are HX-Trigger, HX-Trigger-After-Settle, and HX-Trigger-After-Swap.
+func (td TriggerDelay) String() string {
+	switch td {
+	case 1:
+		return "HX-Trigger-After-Settle"
+	case 2:
+		return "HX-Trigger-After-Swap"
+	default:
+		return "HX-Trigger"
+	}
+}
+
+// Trigger creates a DecoratorFunction that adds a trigger header to the response.
+// The when parameter specifies when the event should be triggered (e.g., immediately, after settle, after swap).
+// The eventName parameter specifies the name of the event(s) to be triggered.
+// Multiple event names can be provided, separated by commas.
+// https://htmx.org/headers/hx-trigger/
+func Trigger(when TriggerDelay, eventName ...string) DecoratorFunction {
 	events := strings.Join(eventName, ", ")
-	return AddCustomHeader("HX-Trigger", events)
+	return AddCustomHeader(when.String(), events)
 }
 
 // TriggerEvent represents an event that can be triggered with additional details.
@@ -18,11 +46,12 @@ type TriggerEvent struct {
 	Detail any    // Additional details associated with the event.
 }
 
-// TriggerWithDetail adds an event JSON object to the response headers.
+// TriggerWithDetail creates a DecoratorFunction that adds an event JSON object to the response headers.
 // The JSON object contains a mapping of event names to their corresponding details.
-// Each TriggerEvent in the arguments specifies an event name and its associated details.
+// The when parameter specifies when the event should be triggered (e.g., immediately, after settle, after swap).
+// The events parameter specifies a slice of TriggerEvent structs, each specifying an event name and its associated details.
 // https://htmx.org/headers/hx-trigger/
-func TriggerWithDetail(events ...TriggerEvent) DecoratorFunction {
+func TriggerWithDetail(when TriggerDelay, events ...TriggerEvent) DecoratorFunction {
 	return func(w http.ResponseWriter) error {
 		eventMap := map[string]interface{}{}
 		for _, event := range events {
@@ -34,7 +63,7 @@ func TriggerWithDetail(events ...TriggerEvent) DecoratorFunction {
 			return err
 		}
 
-		w.Header().Set("HX-Trigger", string(jsonData))
+		w.Header().Set(when.String(), string(jsonData))
 		return nil
 	}
 }
